80d3d6c9e6aa832e0e62b2c38ce1c5ff3995850d
github zuoboo <nsnxn1022@gmail.com>
2022-12-16 10:47:40 +0900

    nav,index

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,5 +6,45 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+
+    //==========ここから追加==========
+    public function index()
+    {
+        // ダミーデータ
+        $articles = [
+            (object) [
+                'id' => 1,
+                'title' => 'タイトル1',
+                'body' => '本文1',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 1,
+                    'name' => 'ユーザー名1',
+                ],
+            ],
+            (object) [
+                'id' => 2,
+                'title' => 'タイトル2',
+                'body' => '本文2',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 2,
+                    'name' => 'ユーザー名2',
+                ],
+            ],
+            (object) [
+                'id' => 3,
+                'title' => 'タイトル3',
+                'body' => '本文3',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 3,
+                    'name' => 'ユーザー名3',
+                ],
+            ],
+        ];
+
+        return view('articles.index', ['articles' => $articles]);
+    }
+    //==========ここまで追加==========
 }

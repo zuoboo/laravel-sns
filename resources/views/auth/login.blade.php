8fe5c0f5674fcc2c5b5b090148aba9537b95abd0
github zuoboo <nsnxn1022@gmail.com>
2022-12-16 15:15:35 +0900

    mail,paddword

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,9 +28,10 @@
                                     <input class="form-control" type="password" id="password" name="password" required>
                                 </div>
 
-                                {{-- ここから --}}
                                 <input type="hidden" name="remember" id="remember" value="on">
-                                {{-- ここまで --}}
+                                <div class="text-left">
+                                    <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                                </div>
 
                                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
 

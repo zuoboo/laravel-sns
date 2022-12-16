704e7993d593a054e7516f09653f3e81fa6486cf
github zuoboo <nsnxn1022@gmail.com>
2022-12-17 08:51:03 +0900

    いいね記事eagerload

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
 
     public function likes(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()->load(['likes.user', 'likes.likes', 'likes.tags']);
 
         $articles = $user->likes->sortByDesc('created_at');
 

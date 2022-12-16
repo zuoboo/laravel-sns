fa13881eaa1befcd7da85df3efc6860de994a70d
github zuoboo <nsnxn1022@gmail.com>
2022-12-17 07:46:35 +0900

    follow一覧表示

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -19,10 +19,10 @@
     </div>
     <div class="card-body">
         <div class="card-text">
-            <a href="" class="text-muted mr-2">
+            <a href="{{ route('users.followings', ['name' => $user->name]) }}" class="text-muted mr-2">
                 {{ $user->count_followings }} フォロー
             </a>
-            <a href="" class="text-muted">
+            <a href="{{ route('users.followers', ['name' => $user->name]) }}" class="text-muted">
                 {{ $user->count_followers }} フォロワー
             </a>
         </div>

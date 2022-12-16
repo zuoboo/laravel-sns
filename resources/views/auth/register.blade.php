4a79074ab952d8b54c1756d47d6036e3ec779dee
github zuoboo <nsnxn1022@gmail.com>
2022-12-17 08:35:25 +0900

    googleAPI導入処理

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,6 +10,9 @@
                 <div class="card mt-3">
                     <div class="card-body text-center">
                         <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
+                        <a href="{{ route('login.{provider}', ['provider' => 'google']) }}" class="btn btn-block btn-danger">
+                            <i class="fab fa-google mr-1"></i>Googleで登録
+                        </a>
                         @include('error_card_list')
                         <div class="card-text">
                             {{-- ここから --}}

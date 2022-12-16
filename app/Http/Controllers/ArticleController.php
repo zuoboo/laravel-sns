b2cb9d30159c110282370ea7e17fafb0fadbed0f
github zuoboo <nsnxn1022@gmail.com>
2022-12-16 14:35:42 +0900

    policy

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,6 +8,10 @@ use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
 
     public function index()
     {
@@ -35,5 +39,22 @@ class ArticleController extends Controller
     public function edit(Article $article)
     {
         return view('articles.edit', compact('article'));
+    }
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
     }
 }

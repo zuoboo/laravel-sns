c1d6f7a8b6e95a72b5e3cdbaef51e90094de8235
github zuoboo <nsnxn1022@gmail.com>
2022-12-16 13:24:28 +0900

    記事投稿画面作成等

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
@@ -15,4 +16,19 @@ class ArticleController extends Controller
         return view('articles.index', ['articles' => $articles]);
     }
 
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        // $article->title = $request->title;
+        // $article->body = $request->body;
+        $article->fill($request->all());
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,6 @@
 |
 */
 Auth::routes();
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'ArticleController@index')->name('articles.index');
+Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+

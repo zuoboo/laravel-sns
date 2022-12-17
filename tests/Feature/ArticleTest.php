d2c01e8663c84d14cce2f39e2016abc66d3e33d4
github zuoboo <nsnxn1022@gmail.com>
2022-12-17 10:20:15 +0900

    いいねテスト2,3

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Article;
+use App\User;
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,6 +20,29 @@ class ArticleTest extends TestCase
         $article = factory(Article::class)->create();
 
         $result = $article->isLikedBy(null);
+
+        $this->assertFalse($result);
+    }
+    // その記事をいいねしているUserモデルのインスタンスを引数として渡した時、trueが返ってくるテスト
+    public function testIsLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $article->likes()->attach($user);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+    // その記事をいいねしていないUserモデルのインスタンスを引数として渡した時、falseが返ってくる
+    public function testIsLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        $article->likes()->attach($another);
+
+        $result = $article->isLikedBy($user);
 
         $this->assertFalse($result);
     }
